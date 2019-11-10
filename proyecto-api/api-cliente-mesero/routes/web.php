2a7e5cf8052815d5cdef88b1d6b5a82d0e3b960a
Christian Alvarez <angel12xd@gmail.com>
2019-11-09 22:31:52 -0600

    Nueva estructura api

--- proyecto-api/api-cliente-mesero/routes/web.php
+++ proyecto-api/api-cliente-mesero/routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/get-platillos', 'Productos@get_platillos')->name('platillos');
+Route::post('/prueba-post', 'Productos@prueba_post')->name('prueba-post');
+Route::get('/prueba-get/{nombre}', 'Productos@prueba_get')->name('prueba-get');
