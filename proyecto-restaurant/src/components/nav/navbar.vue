e5888239d7a1bf85be338489c9e096dda8fede95
Christian Alvarez <angel12xd@gmail.com>
2019-11-11 00:41:00 -0600

    Avance index restaurant

--- proyecto-restaurant/src/components/nav/navbar.vue
+++ proyecto-restaurant/src/components/nav/navbar.vue
@@ -1,39 +1,48 @@
 <template>
   <div>
-    <header class="header">
-      <nav class="navbar navbar-expand-lg navbar-dark bg-light">
-        <a class="navbar-brand" href="#">
-          <h3>Restaurant ®</h3>
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
+    <header class="nav-wrapper">
+      <div class="fixed-top">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-light">
+          <a class="navbar-brand" href="#">
+            <img width="150px" src="/assets/dist/images/brand.png">
+            <!-- <h3>Restaurant ®</h3> -->
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Menú</a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#">Acerca de</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Iniciar sesion</a>
-            </li>
-          </ul>
-          <!-- <form class="form-inline my-2 my-lg-0">
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">
+                  <h4>Home</h4>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <h4>Menú</h4>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <h4>Promociones</h4>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#">
+                  <h4>Acerca de</h4>
+                </a>
+              </li>
+            </ul>
+            <!-- <form class="form-inline my-2 my-lg-0">
             <input
               class="form-control mr-sm-2"
               type="search"
@@ -41,16 +50,17 @@
               aria-label="Search"
             />
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>-->
-        </div>
-        <form class="form-inline">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Iniciar Sesion</a>
-            </li>
-          </ul>
-        </form>
-      </nav>
+            </form>-->
+          </div>
+          <form class="form-inline">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Iniciar Sesion</a>
+              </li>
+            </ul>
+          </form>
+        </nav>
+      </div>
     </header>
   </div>
 </template>
@@ -60,13 +70,25 @@ export default {};
 </script>
 
 <style scoped>
-.header {
+/* .header {
   background: url(/assets/dist/images/wallpaper.jpg) no-repeat center;
   background-size: cover;
   min-height: 100vh;
+}  */
+
+.navbar {
+  /* background-color: transparent !important; */
+  background-color: rgba(0, 0, 0, 0.2) !important;
+}
+.fixed-top {
+  position: absolute;
+  /* top: 0; */
+  /* right: 0; */
+  /* left: 0; */
+  z-index: 1030;
 }
 
-.header .navbar {
-  background-color: transparent !important;
+h4 {
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif !important;
 }
 </style>
--- proyecto-restaurant/src/views/Index.vue
+++ proyecto-restaurant/src/views/Index.vue
@@ -1,22 +1,49 @@
 <template>
   <div>
-      <navbar></navbar>
-      <!-- <img class="img-fluid" src="/assets/dist/images/wallpaper.jpg"> -->
+    <navbar></navbar>
+    <!-- <img class="img-fluid" src="/assets/dist/images/wallpaper.jpg"> -->
+    <div class="row img-bg m-0">
+      <!-- <img src="/assets/dist/images/wallpaper.jpg" /> -->
+      <!-- <h5>asdasd</h5> -->
+      <div class="col-md-12">
+        <div class="row mt-5">
+          <div class="col-sm-12 col-md-12 col-lg-12 col-xg-12 text-center mt-5">
+            <h1 class="display-2 text-light">La comida más fresca</h1>
+            <h1 class="display-5 text-light">Al precio más accesible</h1>
+            <h1 class="display-5 text-light mt-5">¡¡Bienvenido!!</h1>
+            <button class="btn btn-outline-light mt-5">
+                <h4 class="m-0">Platillos</h4>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <acerca-de></acerca-de>
   </div>
 </template>
 
 <script>
-import Navbar from '@/components/nav/navbar'
+import Navbar from "@/components/nav/navbar";
+import AcercaDe from "@/components/index/acerca-de"
 
 export default {
-    components : {
-        Navbar
-    }
+  components: {
+    Navbar,
+    AcercaDe
+  }
 };
 </script>
 
 <style scoped>
-    img {
-        position: relative;
-    }
+.img-bg {
+  background: url(/assets/dist/images/wallpaper.jpg) no-repeat center;
+  /* min-height: 80vh !important; */
+  width: 100% !important;
+  height: 100vh;
+}
+
+h1 {
+    font-family: 'Times New Roman', Times, serif;
+    text-shadow: 0px 0px 5px #000;
+}
 </style>
