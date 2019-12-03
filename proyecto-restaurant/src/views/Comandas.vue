2b4c3fc7919c4934aa7058f61fb9f9dc6ff6673b
Juan Silvestre Ramirez Becerra <trebol_silvestre14@hotmail.com>
2019-12-03 11:04:05 -0600

    comandas modified

--- proyecto-restaurant/src/views/Comandas.vue
+++ proyecto-restaurant/src/views/Comandas.vue
@@ -1,19 +1,25 @@
 <template>
   <div>
-      <nav-bar></nav-bar>
-        
+    <nav-bar></nav-bar>
+    <div>
+      <h4>Comanda</h4>
+      <b-card-group deck>
+        <b-card img-src="https://placekitten.com/1000/300" img-alt="Card image" img-top>
+          <b-card-text>Comanda contenido</b-card-text>
+        </b-card>
+      </b-card-group>
+    </div>
   </div>
 </template>
 
 <script>
-import NavBar from '@/components/nav/navbar'
+import NavBar from "@/components/nav/navbar";
 export default {
-    components: {
-        NavBar
-    }
-}
+  components: {
+    NavBar
+  }
+};
 </script>
 
 <style>
-
 </style>
