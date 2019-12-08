cdaa7556726e5f4fcc8f2dbec01d0118a0791e1a
Christian Alvarez <angel12xd@gmail.com>
2019-12-07 23:23:52 -0600

    Modal para inicio de sesion

--- proyecto-restaurant/src/components/nav/navbar.vue
+++ proyecto-restaurant/src/components/nav/navbar.vue
@@ -46,7 +46,7 @@
               <a href="blog.html">Contacto</a>
             </li>
             <li>
-              <a href="" data-toggle="modal" data-target="#modal-1">Iniciar sesion</a>
+              <a @click="$refs.modal_session.open()">Iniciar sesion</a>
             </li>
           </ul>
           <div class="extra-text visible-xs">
@@ -75,15 +75,34 @@
         </nav>
       </div>
     </header>
-    <modal-session></modal-session>
+    <!-- <modal-session></modal-session> -->
+    <sweet-modal ref="modal_session" title="Iniciar Sesion" overlay-theme="dark">
+      <div class="form-group row">
+        <label for="staticEmail" class="col-sm-2 col-form-label">Usuario</label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" id="staticEmail" />
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="inputPassword" class="col-sm-2 col-form-label">Contraseña</label>
+        <div class="col-sm-10">
+          <input type="password" class="form-control" id="inputPassword" placeholder="Contraseña" />
+        </div>
+      </div>
+
+      <button slot="button" class="btn btn-danger" @click="$refs.modal_session.close()">Cerrar</button>
+      <button slot="button" class="btn btn-success">Aceptar</button>
+    </sweet-modal>
   </div>
 </template>
 
 <script>
-import ModalSession from '@/components/modal/modal-sesion';
+// import ModalSession from '@/components/modal/modal-sesion';
+import { SweetModal } from 'sweet-modal-vue'
+
 export default {
   components : {
-    ModalSession
+    SweetModal
   },
   data(){
     return{
